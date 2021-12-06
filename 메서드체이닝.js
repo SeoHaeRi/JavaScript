21b8743ed7a175ba7913c7dc322bb36c26e2c36c
SeoHaeRi <62414262+SeoHaeRi@users.noreply.github.com>
2021-12-06 22:08:38 +0900

    day 26

---
+++ 메서드체이닝.js
@@ -0,0 +1,12 @@
+Array(100)
+    .fill(0)
+    .map((value, index) => (index + 1) ** 2);
+
+let sum = 0;
+"12367"
+    .split("")
+    .map((value) => parseInt(value))
+    .forEach((value) => (sum += value));
+
+let today = new Date("2021/12/5/12:00");
+today.toISOString();
